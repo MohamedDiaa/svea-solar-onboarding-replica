503a20502fce95605225a8b84825706a857c1ab1
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-25 18:34:32 +0100

    Initial Commit

---
+++ SveaSolar/SveaSolarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SveaSolarApp.swift
+//  SveaSolar
+//
+//  Created by Mohamed Alwakil on 2025-01-25.
+//
+
+import SwiftUI
+
+@main
+struct SveaSolarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
