31992cf413a00546ae78bd0315ee4f3086007994
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-25 19:04:35 +0100

    add appinfo view

---
+++ SveaSolar/Screen/Onboarding/AppInfoView.swift
@@ -0,0 +1,62 @@
+//
+//  AppInfoView.swift
+//  SveaSolar
+//
+//  Created by Mohamed Alwakil on 2025-01-25.
+//
+
+import SwiftUI
+
+struct AppInfoView: View {
+    var body: some View {
+
+
+        VStack(alignment: .leading) {
+
+            Text("App Info")
+                .padding()
+
+            VStack {
+                Text("This app is for Svea Solar customers. If you have not yet started your journey towards a more sustainable life. you can do it here:")
+
+                Button {
+
+                } label: {
+                    Text("Visit our website")
+                        .foregroundStyle(.white)
+                        .padding(.vertical,10)
+                }
+                .frame(maxWidth: .infinity)
+                .background(.black, in: Capsule())
+            }
+            .padding()
+            .padding(.vertical, 20)
+            .background(.gray.opacity(0.1), in: .rect(cornerRadius: 10))
+
+
+            VStack(alignment: .leading,spacing: 20) {
+                Text("This app is for Svea Solar customers. If you have not yet started your journey towards a more sustainable life. you can do it here:")
+
+                Button {
+
+                } label: {
+                    Text("Contact supprt")
+                        .foregroundStyle(.black)
+                        .underline(true,color: .black)
+                }
+
+            }
+            .padding()
+            .padding(.vertical, 20)
+            .background(.gray.opacity(0.1), in: .rect(cornerRadius: 10))
+
+            Spacer(minLength: 0)
+        }
+        .padding()
+
+    }
+}
+
+#Preview {
+    AppInfoView()
+}
